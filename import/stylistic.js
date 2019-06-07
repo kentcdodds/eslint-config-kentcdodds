df8d7b734595406f51fc792b0060e565f428f33f
Kent C. Dodds <kent@doddsfamily.us>
2019-06-07 14:06:25 -0600

    fix: disable import/newline-after-import
    Because it's a bad rule.

--- import/stylistic.js
+++ import/stylistic.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: './non-rules-config.js',
   rules: {
     'import/extensions': 'error',
-    'import/newline-after-import': 'error',
+    'import/newline-after-import': 'off',
     'import/no-named-as-default': 'error',
     'import/no-amd': 'error',
     'import/no-commonjs': 'off',
