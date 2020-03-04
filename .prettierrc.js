87f6d7b1bc5ea6c521df0915de6c2bde6826ed96
Michaël De Boey <info@michaeldeboey.be>
2020-03-04 22:00:57 +0100

    chore: Update dependencies

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,1 @@
-module.exports = {
-  bracketSpacing: false,
-  endOfLine: 'lf',
-  proseWrap: 'always',
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'all',
-}
+module.exports = require('kcd-scripts/prettier')
