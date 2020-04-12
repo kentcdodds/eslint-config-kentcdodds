dfdb67cc7245ab635e2b3ce1f4636eaadb522c21
Michaël De Boey <info@michaeldeboey.be>
2020-04-13 01:03:56 +0200

    refactor: Extract deprecated rules into it's own file

---
+++ deprecated-rules.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: './non-rules-config.js',
+  rules: {
+    'lines-around-directive': 'off',
+    'newline-after-var': 'off',
+    'newline-before-return': 'off',
+    'no-catch-shadow': 'off',
+    'no-native-reassign': 'off',
+    'no-negated-in-lhs': 'off',
+    'require-jsdoc': 'off',
+    'valid-jsdoc': 'off',
+  },
+}
