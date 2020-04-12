0c12ef985549aaaa844fe5d7272fa421197b1f2e
Michaël De Boey <info@michaeldeboey.be>
2020-04-12 19:11:42 +0200

    refactor: Extract deprecated babel rules into it's own file

--- es6/stylistic.js
+++ es6/stylistic.js
@@ -27,15 +27,5 @@ module.exports = {
 
     'valid-typeof': 'off',
     'babel/valid-typeof': 'error',
-
-    //   no eslint version
-    'babel/flow-object-type': 'off', // no opinion
-
-    // deprecated rules
-    'babel/array-bracket-spacing': 'off',
-    'babel/arrow-parens': 'off',
-    'babel/func-params-comma-dangle': 'off',
-    'babel/generator-star-spacing': 'off',
-    'babel/object-shorthand': 'off',
   },
 }
