25235ad4541dc96bc45dd0ab7fe68e97b7d9f023
Michaël De Boey <info@michaeldeboey.be>
2020-04-13 03:21:32 +0200

    feat: Enable TypeScript linting
    BREAKING CHANGE: Requires ESLint@^7.5.x
    BREAKING CHANGE: (Optionally) requires TypeScript@^4.x

--- es6/non-rules-config.js
+++ es6/non-rules-config.js
@@ -9,4 +9,16 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['babel'],
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {},
+    },
+  ],
 }
--- import/non-rules-config.js
+++ import/non-rules-config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  plugins: ['import'],
   env: {
     es6: true,
   },
@@ -7,7 +6,21 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['import'],
   settings: {
     'import/ignore': ['node_modules', '.json$', '.(scss|less|css|styl)$'],
   },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      extends: 'plugin:import/typescript',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {},
+    },
+  ],
 }
