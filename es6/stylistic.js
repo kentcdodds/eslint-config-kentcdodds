f3bea592e4522637ecc605cd519f1fd0cca0ff0d
Michaël De Boey <info@michaeldeboey.be>
2020-04-15 02:16:42 +0200

    refactor: Remove all Prettier rules

--- es6/stylistic.js
+++ es6/stylistic.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: './non-rules-config.js',
   rules: {
-    'arrow-body-style': 'off', // this is nice, but not useful all the time.
     'object-shorthand': ['error', 'properties'], // methods are optional so you can specify a name if you want
     'prefer-arrow-callback': [
       'error',
