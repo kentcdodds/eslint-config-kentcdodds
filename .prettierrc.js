9137e63161b13ab79df090f4e8d23814a03d3103
Kent C. Dodds <me+github@kentcdodds.com>
2020-09-21 10:48:53 -0600

    chore: fix prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,19 @@
-module.exports = require('kcd-scripts/prettier')
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
