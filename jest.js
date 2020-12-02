8a53ff5cdf15bf2419a84f37b0892323bba1a6d7
Kent C. Dodds <me+github@kentcdodds.com>
2020-12-02 12:29:07 -0700

    feat(jest): add jest-dom/prefer-to-have-value

--- jest.js
+++ jest.js
@@ -90,6 +90,7 @@ module.exports = {
           'jest-dom/prefer-to-have-attribute': 'error',
           'jest-dom/prefer-to-have-style': 'error',
           'jest-dom/prefer-to-have-text-content': 'error',
+          'jest-dom/prefer-to-have-value': 'error',
         }
       : null),
 
