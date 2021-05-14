3d6f5a612d210036dbf1a70bca82d6b8a027236e
Michaël De Boey <info@michaeldeboey.be>
2021-05-15 01:51:50 +0200

    feat: update dependencies & add new rules

--- es6/index.js
+++ es6/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
     './non-rules-config.js',
-    './deprecated-rules.js',
     './stylistic.js',
     './best-practices.js',
     './possible-errors.js',
--- es6/possible-errors.js
+++ es6/possible-errors.js
@@ -9,10 +9,7 @@ module.exports = {
     'no-unsafe-optional-chaining': 'error',
 
     'no-unused-expressions': 'off',
-    'babel/no-unused-expressions': 'error',
-
-    'valid-typeof': 'off',
-    'babel/valid-typeof': 'error',
+    '@babel/no-unused-expressions': 'error',
   },
   overrides: [
     {
@@ -22,12 +19,11 @@ module.exports = {
         'no-const-assign': 'off', // ts(2588)
         'no-new-symbol': 'off', // ts(2588)
         'no-this-before-super': 'off', // ts(2376)
-        'babel/valid-typeof': 'off', // ts(2367)
 
         'no-dupe-class-members': 'off',
         '@typescript-eslint/no-dupe-class-members': 'off', // ts(2393) & ts(2300)
 
-        'babel/no-unused-expressions': 'off',
+        '@babel/no-unused-expressions': 'off',
         '@typescript-eslint/no-unused-expressions': 'error',
       },
     },
