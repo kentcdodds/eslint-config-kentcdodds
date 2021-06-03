c609ea46609d076a8b3caaaefeee6f57b74e8fe2
Kent C. Dodds <me+github@kentcdodds.com>
2021-06-03 14:45:12 -0600

    fix(jest): only apply jest rules to jest files

--- samples/.eslintrc.js
+++ samples/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['../react.js', '../jsx-a11y.js', '../index.js'],
+  extends: ['../react.js', '../jsx-a11y.js', '../index.js', '../jest.js'],
   overrides: [
     {
       files: ['**/*.ts?(x)'],
---
+++ samples/__tests__/index.js
@@ -0,0 +1,4 @@
+test('this is a test file', () => {
+  // just to make sure the jest config works
+  expect(1).toBe(1)
+})
