4ec2cf46cafdaadd0050ae4957a78bdeb5697851
Kent C. Dodds <me+github@kentcdodds.com>
2021-07-20 11:00:03 -0600

    fix: disable sort-type-union-intersection-members

--- stylistic.js
+++ stylistic.js
@@ -87,7 +87,7 @@ module.exports = {
         '@typescript-eslint/no-type-alias': 'off',
         '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'warn',
         '@typescript-eslint/prefer-for-of': 'off', // I prefer for of, but I don't want to lint for it
-        '@typescript-eslint/sort-type-union-intersection-members': 'warn',
+        '@typescript-eslint/sort-type-union-intersection-members': 'off',
         '@typescript-eslint/typedef': 'off',
       },
     },
